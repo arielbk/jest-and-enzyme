56e71967c22c5b91b3a9b39716e300dff0d87f4f
arielbk <buchwald.kearney@gmail.com>
2019-03-29 17:51:15 +0100

    Unit testing a simple counter app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import { hot } from 'react-hot-loader';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <h1>
-          Hello, worlddd
-        </h1>
+      <div className="App" data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        <button
+          data-test="increment-button"
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          Increment counter
+        </button>
       </div>
     )
   }
