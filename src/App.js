0974ed56e0dc580af1ee006c663d9675a6942ade
arielbk <buchwald.kearney@gmail.com>
2019-03-31 20:31:34 +0200

    Add CORRECT_GUESS action creator and tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+
 import './App.css';
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" data-test="component-app">
-        <h1>Hello, world!</h1>
+      <div className="container" data-test="component-app">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[
+          { guessedWord: 'train', letterMatchCount: 3 }
+        ]} />
       </div>
-    )
-  }
+    );
+  };
 }
 
 export default hot(module)(App);
