f0eee49c9b9cc2786ab5f16a5b40bfa2786c2612
arielbk <buchwald.kearney@gmail.com>
2019-03-31 15:14:58 +0200

    Add prop checking on congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Functional react component for congrats message
  * @function
+ * @param {object} props - React props
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false)
  */
-export default () => {
-  return (
-    <div />
-  );
+const Congrats = (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations, you guessed the word!
+        </span>
+      </div>
+    )
+  } else {
+    return (
+      <div data-test="component-congrats" />
+    )
+  }
+};
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
 }
+
+export default Congrats;
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,4 @@
+import checkPropTypes from 'check-prop-types';
 
 /**
  * Return node(s) with the given data-test attribute
@@ -8,3 +9,16 @@
 export const findByTestAttr = (wrapper, val) => (
   wrapper.find(`[data-test='${val}']`)
 );
+
+/**
+ * 
+ */
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
+  expect(propError).toBeUndefined();
+}
