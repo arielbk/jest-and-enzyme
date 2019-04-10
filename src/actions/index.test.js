56a14d1e309e4ca218df2d9052249c25ac80aef0
arielbk <buchwald.kearney@gmail.com>
2019-04-10 22:19:29 +0200

    Add secretWord test, action creator && reducer

---
+++ src/actions/index.test.js
@@ -0,0 +1,33 @@
+import moxios from 'moxios';
+
+import { storeFactory } from '../../test/testUtils';
+import { getSecretWord } from './';
+
+describe('getSecretWord action creator', () => {
+  beforeEach(() => {
+    // axios instance would go here if it was used in this app
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+  test('adds response word to state', () => {
+    const secretWord = 'party';
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      });
+    });
+
+    // getSecretWord() returns a function that returns a promise...
+    return store.dispatch(getSecretWord())
+      .then(() => {
+        const newState = store.getState();
+        expect(newState.secretWord).toBe(secretWord);
+      });
+  });
+});
